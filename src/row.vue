27ebb7c698f7d9bb40e8abe763bdb7e524af7c56
月光下的恋人 <15323771186@163.com>
2018-12-03 20:24:39 +0800

    基本实现col&row的功能

--- src/row.vue
+++ src/row.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight: -gutter/2}+'px'">
     <slot></slot>
   </div>
 </template>
 <script>
+export default {
+  name: "GuluRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  created() {},
+  mounted() {
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter;
+    });
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .row {
   display: flex;
+  margin: 0 -10px;
 }
 </style>
