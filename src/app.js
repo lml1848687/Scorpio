5b6d593e449871e9af91352b3a0caee5de3becec
月光下的恋人 <15323771186@163.com>
2018-12-08 12:13:40 +0800

    解决currentToast的一个bug

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ new Vue({
   methods:{
     showToast(){
       this.$toast('你的智商需要充值!', {
-        position: 'middle',
+        position: 'bottom',
         enableHtml: false,
         closeButton:{
           text:'已充值',
--- src/plugin.js
+++ src/plugin.js
@@ -10,7 +10,14 @@ export default {
       if (currentToast) {
         currentToast.close()
       }
-      currentToast = createToast({Vue,message,propsData:toastOptions})
+      currentToast = createToast({
+        Vue,
+        message,
+        propsData:toastOptions,
+        onClose:()=>{
+          currentToast = null
+        }
+      })
     }
   }
 }
@@ -19,13 +26,14 @@ export default {
 
 
 /*helper */
-function createToast({ Vue,message, propsData }) {
+function createToast({ Vue,message, propsData,onClose }) {
   let Constructor = Vue.extend(Toast)
   let toast = new Constructor({
     propsData
   })
   toast.$slots.default = [message]
   toast.$mount()
+  toast.$on('close',onClose)
   document.body.appendChild(toast.$el)
   return toast
 }
