b8d8cd0fdf7d01c6651e6308a69822bd52566115
月光下的恋人 <15323771186@163.com>
2018-12-08 11:50:10 +0800

    实现position:ytop/top/middle

--- src/app.js
+++ src/app.js
@@ -35,10 +35,21 @@ new Vue({
     loading1: false,
     loading2: true,
     loading3: false,
-    message: 'hidadadawdawd'
+    message: 'hi'
   },
   created(){
-    this.$toast(' <strong>我知道了我知道了我知道了我知道了我知道了我知道了我知道了我知道了我知道了</strong> ', {enableHtml: true})
+    this.$toast('你的智商需要充值!', {
+      position: 'middle',
+      enableHtml: false,
+      closeButton:{
+        text:'已充值',
+        callback(){
+          console.log('他说已经充值智商了')
+        }
+      },
+      autoClose: false,
+      autoCloseDelay: 3
+    })
   },
   methods:{
     showToast(){
