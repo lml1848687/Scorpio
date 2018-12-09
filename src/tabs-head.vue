b963860bc8fd40ad1e2655d07122f7a0b91866b8
月光下的恋人 <15323771186@163.com>
2018-12-09 12:39:29 +0800

    触发update:selected事件的时候 添加一个item数据

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
+    <div class="line" ref="line"></div>
     <div class="actions-wrapper">
       <slot name='actions'></slot>
     </div>
@@ -11,19 +12,29 @@
     name:'GuluTabsHead',
     inject:['eventBus'],
     created() {
-      this.$emit('update:selected', '这是this $emit出来的')
-      
+      this.eventBus.$on('update:selected',(item,vm)=>{
+        console.log(item)
+        console.log(vm)
+      })
     },
   }
 </script>
 <style lang="scss" scoped>
 $tab-height: 40px;
+$blue:blue;
 .tabs-head {
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
   align-items: center;
   border: 1px solid red;
+  position: relative;
+  >.line{
+    position: absolute;
+    bottom: 0;
+    border-bottom: 1px solid $blue;
+    width: 100px;
+  }
   >.actions-wrapper {
     margin-left: auto;
   }
--- src/tabs.vue
+++ src/tabs.vue
@@ -4,39 +4,50 @@
   </div>
 </template>
 <script>
-import Vue from 'vue'
-  export default {
-    name:'GuluTabs',
-    props:{
-      selected:{
-        type: String,
-        required: true
-      },
-      direction:{
-        type: String,
-        default:'horizontal',
-        validator(value){
-          return ['horizontal','vertical'].indexOf(value) >= 0
-        }
+import Vue from "vue";
+export default {
+  name: "GuluTabs",
+  props: {
+    selected: {
+      type: String,
+      required: true
+    },
+    direction: {
+      type: String,
+      default: "horizontal",
+      validator(value) {
+        return ["horizontal", "vertical"].indexOf(value) >= 0;
       }
-    },
-    data(){
-      return {
-        eventBus: new Vue()
+    }
+  },
+  data() {
+    return {
+      eventBus: new Vue()
+    };
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus
+    };
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      if (vm.$options.name === "GuluTabsHead") {
+        vm.$children.forEach(item => {
+          if (
+            item.$options.name === "GuluTabsItem" &&
+            item.name === this.selected
+          ) {
+            console.log(item.$el)
+            this.eventBus.$emit("update:selected", this.selected,item);
+          }
+        });
       }
-    },
-    provide(){
-      return {
-        eventBus: this.eventBus
-      }
-    },
-    mounted() {
-      this.eventBus.$emit('update:selected',this.selected)
-    },
+    });
   }
+};
 </script>
 <style lang="scss" scoped>
 .tabs {
-
 }
 </style>
