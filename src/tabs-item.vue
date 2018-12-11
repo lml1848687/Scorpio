d594d8efd535f7a1163bde033f2d0fe78127345b
月光下的恋人 <15323771186@163.com>
2018-12-11 19:56:23 +0800

    添加了部分测试用例

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,57 +1,62 @@
 <template>
-  <div class="tabs-item" @click="onClick" :class='classes'>
+  <div class="tabs-item" @click="onClick" :class="classes" :data-name="name">
     <slot></slot>
   </div>
 </template>
 <script>
-  export default {
-    name:'GuluTabsItem',
-    inject:['eventBus'],
-    data(){
+export default {
+  name: "GuluTabsItem",
+  inject: ["eventBus"],
+  data() {
+    return {
+      active: false
+    };
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    name: {
+      type: String | Number,
+      required: true
+    }
+  },
+  computed: {
+    classes() {
       return {
-        active: false
-      }
-    },
-    props:{
-      disabled:{
-        type: Boolean,
-        default: false
-      },
-      name:{
-        type:String | Number,
-        required: true
-      }
-    },
-    computed:{
-      classes(){
-        return {
-          active :this.active,
-          disabled: this.disabled
-        }
-      }
-    },
-    created() {
-      this.eventBus.$on('update:selected',(name)=>{
+        active: this.active,
+        disabled: this.disabled
+      };
+    }
+  },
+  created() {
+    if (this.eventBus) {
+      this.eventBus.$on("update:selected", name => {
         if (name === this.name) {
-          this.active = true
-        }else{
-          this.active = false
+          this.active = true;
+        } else {
+          this.active = false;
         }
-      })
-    },
-    methods:{
-      onClick(){
-        if (this.disabled) {
-          return
-        }
-        this.eventBus.$emit('update:selected',this.name,this)
+      });
+    }
+  },
+  methods: {
+    onClick() {
+      if (this.disabled) {
+        return;
       }
+      if (this.eventBus) {
+        this.eventBus.$emit("update:selected", this.name, this);
+      }
+      this.$emit("click", this);
     }
   }
+};
 </script>
 <style lang="scss" scoped>
-  $blue: blue;
-  $disabled-text-color:grey;
+$blue: blue;
+$disabled-text-color: grey;
 .tabs-item {
   flex-shrink: 0;
   padding: 0 2em;
@@ -60,7 +65,7 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  &.active{
+  &.active {
     color: $blue;
     font-weight: bold;
   }
