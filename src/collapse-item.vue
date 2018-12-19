9fe8e4afed66e7160df3bf0abdba7869c5a25cca
月光下的恋人 <15323771186@163.com>
2018-12-19 20:53:16 +0800

    可以设置默认selected

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -13,6 +13,10 @@ export default {
     title: {
       type: String,
       required: true
+    },
+    name: {
+      type: String,
+      required: true
     }
   },
   data() {
@@ -22,23 +26,28 @@ export default {
   },
   inject: ["eventBus"],
   mounted() {
-    this.eventBus && this.eventBus.$on("update:selected", (vm) => {
-      if (vm !== this) {
-        this.close();
-      }
-    });
+    this.eventBus &&
+      this.eventBus.$on("update:selected", name => {
+        if (name !== this.name) {
+          this.close();
+        } else {
+          this.show();
+        }
+      });
   },
   methods: {
     toggle() {
       if (this.open) {
         this.open = false;
       } else {
-        this.open = true;
-        this.eventBus && this.eventBus.$emit("update:selected", this);
+        this.eventBus && this.eventBus.$emit("update:selected", this.name);
       }
     },
     close() {
       this.open = false;
+    },
+    show() {
+      this.open = true;
     }
   }
 };
--- src/collapse.vue
+++ src/collapse.vue
@@ -11,6 +11,9 @@ export default {
     single: {
       type: Boolean,
       default: false
+    },
+    selected:{
+      type:String
     }
   },
   data() {
@@ -19,12 +22,15 @@ export default {
     };
   },
   provide() {
-    if (this.single) {
+    
       return {
         eventBus: this.eventBus
-      };
-    }
-  }
+      }
+    
+  },
+  mounted() {
+    this.eventBus.$emit('update:selected',this.selected)
+  },
 };
 </script>
 <style lang="scss" scoped>
