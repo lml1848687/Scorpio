95d93875335440c01ea81c9c35eeda7c203f9064
月光下的恋人 <15323771186@163.com>
2018-12-19 20:27:04 +0800

    collapse 基本样式和功能完成

--- src/app.js
+++ src/app.js
@@ -23,6 +23,10 @@ import TabsItem from './tabs-item'
 
 import Popover from './popover'
 
+import Collapse from './collapse'
+import CollapseItem from './collapse-item'
+
+
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -42,6 +46,8 @@ Vue.component('g-tabs-body', TabsBody)
 Vue.component('g-tabs-item', TabsItem)
 Vue.component('g-tabs-pane', TabsPane)
 Vue.component('g-popover', Popover)
+Vue.component('g-collapse', Collapse)
+Vue.component('g-collapse-item', CollapseItem)
 Vue.use(plugin)
 new Vue({
   el: '#app',
