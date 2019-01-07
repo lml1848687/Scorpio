2937df6ee121f599f23a9fb910844cab587f761e
月光下的恋人 <15323771186@163.com>
2019-01-07 20:45:25 +0800

    简单实现cascader

--- src/app.js
+++ src/app.js
@@ -26,60 +26,77 @@ import Popover from './popover'
 import Collapse from './collapse'
 import CollapseItem from './collapse-item'
 
+import Cascader from './cascader'
 
 
-Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
-Vue.component('g-button-group', ButtonGroup)
-Vue.component('g-input', Input)
-Vue.component('g-row', Row)
-Vue.component('g-col', Col)
-Vue.component('g-layout', Layout)
-Vue.component('g-header', Header)
-Vue.component('g-sider', Sider)
-Vue.component('g-footer', Footer)
-Vue.component('g-content', Content)
-Vue.component('g-toast', Toast)
-Vue.component('g-tabs', Tabs)
-Vue.component('g-tabs-head', TabsHead)
-Vue.component('g-tabs-body', TabsBody)
-Vue.component('g-tabs-item', TabsItem)
-Vue.component('g-tabs-pane', TabsPane)
-Vue.component('g-popover', Popover)
-Vue.component('g-collapse', Collapse)
-Vue.component('g-collapse-item', CollapseItem)
-Vue.use(plugin)
+Vue.component('g-button', Button);
+Vue.component('g-icon', Icon);
+Vue.component('g-button-group', ButtonGroup);
+Vue.component('g-input', Input);
+Vue.component('g-row', Row);
+Vue.component('g-col', Col);
+Vue.component('g-layout', Layout);
+Vue.component('g-header', Header);
+Vue.component('g-sider', Sider);
+Vue.component('g-footer', Footer);
+Vue.component('g-content', Content);
+Vue.component('g-toast', Toast);
+Vue.component('g-tabs', Tabs);
+Vue.component('g-tabs-head', TabsHead);
+Vue.component('g-tabs-body', TabsBody);
+Vue.component('g-tabs-item', TabsItem);
+Vue.component('g-tabs-pane', TabsPane);
+Vue.component('g-popover', Popover);
+Vue.component('g-collapse', Collapse);
+Vue.component('g-collapse-item', CollapseItem);
+Vue.component('g-cascader', Cascader);
+Vue.use(plugin);
 new Vue({
-  el: '#app',
-  data: {
-    selectedTab:['2']
-  },
-  created(){
-    
-  },
-  methods:{
-    showToast1(){
-      this.showToast('top')
+    el: '#app',
+    data: {
+        source: [{
+            name: '浙江',
+            children: [
+                {
+                    name: '杭州',
+                    children: [
+                        {name: '上城'},
+                        {name: '下城'},
+                        {name: '江干'},
+                    ]
+                },
+                {
+                    name: '嘉兴',
+                    children: [
+                        {name: '南湖'},
+                        {name: '秀洲'},
+                        {name: '嘉善'},
+                    ]
+                }]
+        }, {
+            name: '福建',
+            children: [
+                {
+                    name: '福州',
+                    children: [
+                        {name: '鼓楼'},
+                        {name: '台江'},
+                        {name: '仓山'},
+                    ]
+                },
+            ]
+        },{
+            name:'安徽',
+            children:[
+                {name:'合肥',
+                    children:[
+                        {name:'瑶海'},
+                    ]
+                },
+            ]
+        }]
     },
-    showToast2(){
-      this.showToast('middle')
-    },
-    showToast3(){
-      this.showToast('bottom')
-    },
-    showToast(position){
-      this.$toast('你的智商需要充值!', {
-        position,
-        enableHtml: false,
-        closeButton:{
-          text:'已充值',
-          callback(){
-            console.log('他说已经充值智商了')
-          }
-        },
-        autoClose: 3,
-      })
-    }
-  }
-})
+
+
+});
 
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,18 +1,88 @@
 <template>
     <div class="cascader">
-        <slot></slot>
+        <div class="trigger" @click="popoverVisible=!popoverVisible">
+        </div>
+        <div class="popover" v-if="popoverVisible">
+            <div class="level1">
+                <div class="label"
+                     v-for="item1 in source"
+                     style="border: 1px solid red"
+                     @click="level1Selected = item1"
+                >
+                    {{item1.name}}
+                </div>
+            </div>
+
+            <div class="label" class="level2">
+                <div v-for="item2 in level2Items"
+                     @click="level2Selected = item2">
+                    {{item2.name}}
+                </div>
+            </div>
+            <div class="label" class="level3" >
+                <div v-for="item3 in level3Items">
+                    {{item3.name}}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import CascaderItem from './cascader-item'
+
     export default {
-        name: "GuluCascader"
+        name: 'GuluCascader',
+        components: {CascaderItem},
+        props: {
+            source: {
+                type: Array
+            }
+        },
+        data() {
+            return {
+                popoverVisible: false,
+                level1Selected:null,
+                level2Selected:null
+            }
+        },
+        computed:{
+            level2Items(){
+                if (this.level1Selected) {
+                    return this.level1Selected.children
+                }else {
+                    return []
+                }
+            },
+            level3Items(){
+                if (this.level2Selected) {
+                    return this.level2Selected.children
+                }else {
+                    return []
+                }
+            }
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
     @import "var";
-.cascader {
 
-}
+    .cascader {
+        .trigger {
+            border: 1px solid red;
+            height: 32px;
+            width: 100px;
+        }
+
+        .popover {
+            border: 1px solid red;
+            height: 200px;
+            width: 200px;
+            display: flex;
+            .label {
+                white-space: nowrap;
+            }
+        }
+    }
 </style>
