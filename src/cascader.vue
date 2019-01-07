5d606c574a84b9546a4a956aac376c1869e24efc
月光下的恋人 <15323771186@163.com>
2019-01-07 23:04:48 +0800

    确定如何渲染内容

--- src/cascader.vue
+++ src/cascader.vue
@@ -3,37 +3,17 @@
         <div class="trigger" @click="popoverVisible=!popoverVisible">
         </div>
         <div class="popover" v-if="popoverVisible">
-            <div class="level1">
-                <div class="label"
-                     v-for="item1 in source"
-                     style="border: 1px solid red"
-                     @click="level1Selected = item1"
-                >
-                    {{item1.name}}
-                </div>
-            </div>
-
-            <div class="label" class="level2">
-                <div v-for="item2 in level2Items"
-                     @click="level2Selected = item2">
-                    {{item2.name}}
-                </div>
-            </div>
-            <div class="label" class="level3" >
-                <div v-for="item3 in level3Items">
-                    {{item3.name}}
-                </div>
-            </div>
+            <cascader-items :items="source"></cascader-items>
         </div>
     </div>
 </template>
 
 <script>
-    import CascaderItem from './cascader-item'
+    import CascaderItems from './cascader-items'
 
     export default {
         name: 'GuluCascader',
-        components: {CascaderItem},
+        components: {CascaderItems},
         props: {
             source: {
                 type: Array
