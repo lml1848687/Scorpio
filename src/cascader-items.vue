cf5451ead7ff8224e1c810944217534fcce6daf1
月光下的恋人 <15323771186@163.com>
2019-01-17 19:51:38 +0800

    parcel 慢起来了

--- src/cascader-items.vue
+++ src/cascader-items.vue
@@ -1,54 +1,75 @@
 <template>
-    <div class="cascaderItems">
-        <div  class="left">
-            <div  class="label"
-                  v-for="item in items"
-                  @click="leftSelected = item">
-                {{item.name}}
-            </div>
-        </div>
-        <div class="right" v-if="rightItems">
-            <gulu-cascader-items :items="rightItems"></gulu-cascader-items>
-        </div>
-    </div>
-
+	<div class="cascaderItems" :style="{height: height}">
+		<div class="left">
+			<div class="label"
+					 v-for="item in items"
+					 @click="leftSelected = item">
+				{{item.name}}
+				<icon v-if="item.children" name="right" class="icon">  </icon>
+			</div>
+		</div>
+		<div class="right" v-if="rightItems">
+			<gulu-cascader-items :items="rightItems" :height="height"></gulu-cascader-items>
+		</div>
+	</div>
 </template>
 
 <script>
-    export default {
-        name: "GuluCascaderItems",
-        props: {
-            items: {
-                type: Array
-            }
-        },
-        data(){
-            return {
-                leftSelected:null
-            }
-        },
-        computed:{
-            rightItems(){
-                if (this.leftSelected && this.leftSelected.children) {
-                    return this.leftSelected.children
-                }else {
-                    return null
-                }
-            }
+	import Icon from './icon'
+  export default {
+    name: "GuluCascaderItems",
+		components:{
+      Icon
+		},
+    props: {
+      items: {
+        type: Array
+      },
+			height:{
+        type: String
+			}
+    },
+    data () {
+      return {
+        leftSelected: null
+      }
+    },
+    computed: {
+      rightItems () {
+        if (this.leftSelected && this.leftSelected.children) {
+          return this.leftSelected.children
+        } else {
+          return null
         }
+      }
     }
+  }
 </script>
 
 <style scoped lang="scss">
-    .cascaderItems {
-        display: flex;
-        align-items: flex-start;
-        justify-content: flex-start;
-        .left {
-            border: 1px solid red;
-        }
-        .right {
-            margin-top: -1px;
-        }
-    }
+	@import "var";
+	.cascaderItems {
+		display: flex;
+		align-items: flex-start;
+		justify-content: flex-start;
+		height: 100px;
+		.left {
+			height: 100%;
+			padding: 0.3em 0;
+		}
+		.right {
+			height: 100%;
+			border-left: 1px solid $border-color-light;
+		}
+		.label {
+			padding: 0.3em 1em;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			.icon {
+				margin-left: 1em;
+				transform: scale(0.8);
+			}
+		}
+	}
 </style>
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,68 +1,74 @@
 <template>
-    <div class="cascader">
-        <div class="trigger" @click="popoverVisible=!popoverVisible">
-        </div>
-        <div class="popover" v-if="popoverVisible">
-            <cascader-items :items="source"></cascader-items>
-        </div>
-    </div>
+	<div class="cascader">
+		<div class="trigger" @click="popoverVisible=!popoverVisible">
+		</div>
+		<div class="popover-wrapper" v-if="popoverVisible">
+			<cascader-items :items="source" class="popover" :height="popoverHeight"></cascader-items>
+		</div>
+	</div>
 </template>
 
 <script>
-    import CascaderItems from './cascader-items'
+  import CascaderItems from './cascader-items'
 
-    export default {
-        name: 'GuluCascader',
-        components: {CascaderItems},
-        props: {
-            source: {
-                type: Array
-            }
-        },
-        data() {
-            return {
-                popoverVisible: false,
-                level1Selected:null,
-                level2Selected:null
-            }
-        },
-        computed:{
-            level2Items(){
-                if (this.level1Selected) {
-                    return this.level1Selected.children
-                }else {
-                    return []
-                }
-            },
-            level3Items(){
-                if (this.level2Selected) {
-                    return this.level2Selected.children
-                }else {
-                    return []
-                }
-            }
+  export default {
+    name: 'GuluCascader',
+    components: {CascaderItems},
+    props: {
+      source: {
+        type: Array
+      },
+			popoverHeight:{
+        type:String
+			}
+    },
+    data() {
+      return {
+        popoverVisible: false,
+        level1Selected:null,
+        level2Selected:null
+      }
+    },
+    computed:{
+      level2Items(){
+        if (this.level1Selected) {
+          return this.level1Selected.children
+        }else {
+          return []
         }
+      },
+      level3Items(){
+        if (this.level2Selected) {
+          return this.level2Selected.children
+        }else {
+          return []
+        }
+      }
     }
+  }
 </script>
 
 <style scoped lang="scss">
-    @import "var";
+	@import "var";
 
-    .cascader {
-        .trigger {
-            border: 1px solid red;
-            height: 32px;
-            width: 100px;
-        }
+	.cascader {
+		position: relative;
+		.trigger {
+			border: 1px solid black;
+			height: 32px;
+			width: 100px;
+		}
 
-        .popover {
-            border: 1px solid red;
-            height: 200px;
-            width: 200px;
-            display: flex;
-            .label {
-                white-space: nowrap;
-            }
-        }
-    }
+		.popover-wrapper {
+			position: absolute;
+			top: 100%;
+			left: 0;
+			background: white;
+			display: flex;
+			@extend .box-shadow;
+			.label {
+				white-space: nowrap;
+			}
+		}
+	}
 </style>
