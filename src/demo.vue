2ca0b149d6a42ebcc10f5c81fdc94d3d13141de7
月光下的恋人 <15323771186@163.com>
2019-01-17 22:42:38 +0800

    use vue-cli to start project

---
+++ src/demo.vue
@@ -0,0 +1,70 @@
+<template>
+	<g-cascader :source="source" popover-height="200px"></g-cascader>
+</template>
+
+<script>
+	import Cascader from './cascader'
+  export default {
+    name: "demo",
+		components:{'g-cascader':Cascader},
+		data(){
+      return {
+          source: [{
+            name: '浙江',
+            children: [
+              {
+                name: '杭州',
+                children: [
+                  {name: '上城'},
+                  {name: '下城'},
+                  {name: '江干'},
+                ]
+              },
+              {
+                name: '嘉兴',
+                children: [
+                  {name: '南湖'},
+                  {name: '秀洲'},
+                  {name: '嘉善'},
+                ]
+              }]
+          }, {
+            name: '福建',
+            children: [
+              {
+                name: '福州',
+                children: [
+                  {name: '鼓楼'},
+                  {name: '台江'},
+                  {name: '仓山'},
+                ]
+              },
+            ]
+          },{
+            name:'安徽',
+            children:[
+              {name:'合肥',
+                children:[
+                  {name:'瑶海'},
+                ]
+              },
+            ]
+          }]
+        }
+			}
+  }
+</script>
+
+<style lang="scss" scoped>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+	html {
+		--font-size: 14px;
+	}
+	body {
+		font-size: var(--font-size);
+	}
+</style>
