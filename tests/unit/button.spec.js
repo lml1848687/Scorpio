426b8378e3decb0a65e31e24931761ad799b288b
月光下的恋人 <15323771186@163.com>
2019-02-16 23:49:10 +0800

    新增2个单元测试

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -1,12 +1,9 @@
 import chai, { expect } from 'chai'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-chai.use(sinonChai)
-
-import { shallowMount, mount } from '@vue/test-utils'
-
+import {mount } from '@vue/test-utils'
 import Button from '@/button.vue'
-import Vue from 'vue'
+chai.use(sinonChai)
 describe('Button.vue', () => {
   it('存在.', () => {
     expect(Button).to.exist
@@ -42,16 +39,14 @@ describe('Button.vue', () => {
     vm.$el.remove()
     vm.$destroy()
   })
-  xit('设置 iconPosition 可以改变 order', () => {
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
+  xit('设置 iconPosition 可以改变 order', () => {    
+    const wrapper = mount(Button, {
       propsData: {
         icon: 'settings',
         iconPosition: 'right'
       }
-    }).$mount(div)
+    })
+    const vm =wrapper.vm
     const icon = vm.$el.querySelector('svg')
     expect(getComputedStyle(icon).order).to.eq('2')
     vm.$el.remove()
