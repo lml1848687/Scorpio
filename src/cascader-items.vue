934c7eb55119dbbf7012217d1c3234f655d5ef7e
月光下的恋人 <15323771186@163.com>
2019-02-16 09:46:19 +0800

    解决左边不渲染的bug

--- src/cascader-items.vue
+++ src/cascader-items.vue
@@ -1,101 +1,105 @@
 <template>
-	<div class="cascaderItems" :style="{height: height}">
-		<div class="left">
-			<div class="label"
-					 v-for="(item,index) in items"
-					 @click="onClickLabel(item)"
-					 :key="index">
-				{{item.name}}
-				<icon v-if="item.children" name="right" class="icon">  </icon>
-			</div>
-		</div>
-		<div class="right" v-if="rightItems">
-			<gulu-cascader-items :items="rightItems" :height="height" :level="level+1"
-			:selected="selected" @update:selected="onUpdateSelected"></gulu-cascader-items>
-		</div>
-	</div>
+  <div class="cascaderItems" :style="{height: height}">
+    <div class="left">
+      <div class="label" v-for="(item,index) in items" @click="onClickLabel(item)" :key="index">
+        {{item.name}}
+        <icon v-if="item.children" name="right" class="icon"></icon>
+      </div>
+    </div>
+    <div class="right" v-if="rightItems">
+      <gulu-cascader-items
+        :items="rightItems"
+        :height="height"
+        :level="level+1"
+        :selected="selected"
+        @update:selected="onUpdateSelected"
+      ></gulu-cascader-items>
+    </div>
+  </div>
 </template>
 
 <script>
-	import Icon from './icon'
-  export default {
-    name: "GuluCascaderItems",
-		components:{
-      Icon
-		},
-    props: {
-      items: {
-        type: Array
-      },
-			height:{
-        type: String
-			},
-			selected:{
-				type: Array,
-				default:()=>{return []}
-			},
-			level:{
-				type:Number,
-				default:0
-			}
+import Icon from "./icon";
+export default {
+  name: "GuluCascaderItems",
+  components: {
+    Icon
+  },
+  props: {
+    items: {
+      type: Array
     },
-    data () {
-      return {
-        
+    height: {
+      type: String
+    },
+    selected: {
+      type: Array,
+      default: () => {
+        return [];
       }
     },
-    computed: {
-      rightItems () {
-				let currentSelected = this.selected[this.level]
-        if (currentSelected && currentSelected.children) {
-          return currentSelected.children
-        } else {
-          return null
+    level: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    rightItems() {
+      if (this.selected[this.level]) {
+        let item = this.items.filter(
+          item => item.name === this.selected[this.level].name
+        );
+        if (item && item[0].children&&item[0].children.length > 0) {
+          return item[0].children;
         }
       }
-		},
-		methods:{
-			onClickLabel(item){
-				//this.seleceted[this.level] = item
-				// this.$set(this.selected,this.level,item)
-				let copy=JSON.parse(JSON.stringify(this.selected))
-				copy[this.level] = item
-				copy.splice(this.level+1)
-				this.$emit('update:selected',copy)
-			},
-			onUpdateSelected(newSelected){
-				this.$emit('update:selected',newSelected)
-			}
-		}
+    }
+  },
+  methods: {
+    onClickLabel(item) {
+      //this.seleceted[this.level] = item
+      // this.$set(this.selected,this.level,item)
+      let copy = JSON.parse(JSON.stringify(this.selected));
+      copy[this.level] = item;
+      copy.splice(this.level + 1);
+      this.$emit("update:selected", copy);
+    },
+    onUpdateSelected(newSelected) {
+      this.$emit("update:selected", newSelected);
+    }
   }
+};
 </script>
 
 <style scoped lang="scss">
-	@import "var";
-	.cascaderItems {
-		display: flex;
-		align-items: flex-start;
-		justify-content: flex-start;
-		height: 100px;
-		.left {
-			height: 100%;
-			padding: 0.3em 0;
-			overflow: auto;
-			border: 1px solid red;
-		}
-		.right {
-			height: 100%;
-			border-left: 1px solid $border-color-light;
-		}
-		.label {
-			padding: 0.3em 1em;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			.icon {
-				margin-left: 1em;
-				transform: scale(0.8);
-			}
-		}
-	}
+@import "var";
+.cascaderItems {
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  height: 100px;
+  .left {
+    height: 100%;
+    padding: 0.3em 0;
+    overflow: auto;
+    border: 1px solid red;
+  }
+  .right {
+    height: 100%;
+    border-left: 1px solid $border-color-light;
+  }
+  .label {
+    padding: 0.3em 1em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .icon {
+      margin-left: 1em;
+      transform: scale(0.8);
+    }
+  }
+}
 </style>
