733db032679dae102f6edbeeda5322d094f04557
月光下的恋人 <15323771186@163.com>
2019-03-04 09:59:20 +0800

    修复toast文档bug

--- docs/.vuepress/components/toast-demo-1.vue
+++ docs/.vuepress/components/toast-demo-1.vue
@@ -17,25 +17,25 @@
   </div>
 </template>
 <style>
-  .gulu-toast {
-    z-index: 30 !important;
-  }
+.gulu-toast {
+  z-index: 30 !important;
+}
 </style>
 <style scoped>
-  * {
-    box-sizing: border-box;
-  }
+* {
+  box-sizing: border-box;
+}
 </style>
 <script>
-  import plugin from '../../../src/plugin'
-  import GButton from '../../../src/button/button'
-  import Vue from 'vue'
-  Vue.use(plugin)
-  export default {
-    components: {GButton},
-    data () {
-      return {
-        content: `
+import plugin from "../../../src/plugin";
+import GButton from "../../../src/button/button";
+import Vue from "vue";
+Vue.use(plugin);
+export default {
+  components: { GButton },
+  data() {
+    return {
+      content: `
           <style>
             .gulu-toast {
               z-index: 30;
@@ -44,8 +44,10 @@
           <g-button @click="$toast('点击弹出提示')">上方弹出</g-button>
           <g-button @click="$toast('点击弹出提示', {position:'middle'})">中间弹出</g-button>
           <g-button @click="$toast('点击弹出提示', {position:'bottom'})">下方弹出</g-button>
-      `.replace(/^ {8}/gm, '').trim()
-      }
-    }
+      `
+        .replace(/^ {8}/gm, "")
+        .trim()
+    };
   }
+};
 </script>
