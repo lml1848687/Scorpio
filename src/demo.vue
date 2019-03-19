f2b618d3ee2dd1cd558b1ee2cb48ce91f7d6afa9
Robert_Liu <liujian891010@gmail.com>
2019-03-19 10:41:12 +0800

    demo empty

--- src/demo.vue
+++ src/demo.vue
@@ -1,27 +1,9 @@
-<template>
-  <div>
-    <s-button>默认按钮</s-button>
-    <s-button icon="settings" icon-position="right">默认按钮</s-button>
-    <s-button :loading="loading" @click="loading=!loading">默认按钮</s-button>
-    <s-button disabled>默认按钮</s-button>
-    <s-button-group>
-      <s-button icon="left">上一页</s-button>
-      <s-button icon="right" icon-position="right">下一页</s-button>
-    </s-button-group>
-  </div>
-</template>
+<template></template>
 <script>
-import Button from "../src/button/button";
-import ButtonGroup from "../src/button/button-group";
-
 export default {
-  name: "demo",
-  components: { "s-button": Button, "s-button-group": ButtonGroup },
-  data() {
-    return {
-      loading: true
-    };
-  }
+  name: "",
+  components: {},
+  data() {}
 };
 </script>
 
