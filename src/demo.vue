d7418d3845d2c152c6e40b0bb5ababd8cffd95ff
萝卜头 <liujian891010@gmail.com>
2019-03-27 22:56:49 +0800

    demo to test

--- src/demo.vue
+++ src/demo.vue
@@ -1,6 +1,15 @@
-<template></template>
+<template>
+  <div>
+    <s-button icon="settings">测试一下</s-button>
+  </div>
+</template>
 <script>
-export default {};
+import Button from "./button/button";
+export default {
+  components: {
+    "s-button": Button
+  }
+};
 </script>
 
 <style lang="scss" scoped>
